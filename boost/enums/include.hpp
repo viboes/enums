bc4daee5ee99f3211b293712500d11a8d2f694a5
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-02-28 00:32:56 +0000

    Enums: Add include.hpp file
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/enums@69362 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/enums/include.hpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2011.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/enums for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_ENUMS_INCLUDE_HPP
+#define BOOST_ENUMS_INCLUDE_HPP
+
+#include <boost/enums/default_value.hpp>
+#include <boost/enums/underlying_type.hpp>
+#include <boost/enums/enum_type.hpp>
+#include <boost/enums/get_value.hpp>
+#include <boost/enums/emmulation.hpp>
+    
+#endif
