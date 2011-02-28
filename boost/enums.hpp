1ea66aedd29de0920453b364ffcf3eda13cf96f3
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-02-28 00:31:09 +0000

    Enums: Add enums.hpp file
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/enums@69361 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/enums.hpp
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2011.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/enums for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_ENUMS_HPP
+#define BOOST_ENUMS_HPP
+
+#include <boost/enums/include.hpp>
+    
+#endif
