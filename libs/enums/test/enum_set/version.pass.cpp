cf5730b109fa793656706b864a1a74d91e2c1d69
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-02 20:31:13 +0000

    Enums: add include test for enum_set
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/enums@69487 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/enums/test/enum_set/version.pass.cpp
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2011.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/enums for documentation.
+//
+// Adapted from libcxx tests
+//
+////////////////////////////////////////////////////////////////////////////////===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <boost/enums/enum_set.hpp>
+
+#include <boost/enums/enum_set.hpp>
+
+//~ #ifndef BOOST_ENUMS_VERSION
+//~ #error BOOST_ENUMS_VERSION not defined
+//~ #endif
+
+int main()
+{
+}
