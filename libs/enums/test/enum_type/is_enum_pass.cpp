db1dd9f1c3f5628677c10e69230aa33fe776cd21
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-19 16:47:46 +0000

    Enums: Added is_enum<> meta-function tests
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/enums@70200 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/enums/test/enum_type/is_enum_pass.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2011.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/enums for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "./EnumType.hpp"
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+void pass() {
+  using namespace boost::enums;
+
+  { // has the same size as the underlying type
+    BOOST_STATIC_ASSERT((boost::enums::is_enum<EnumClass>::value));
+    BOOST_STATIC_ASSERT((!boost::enums::is_enum<int>::value));
+  }
+
+}
