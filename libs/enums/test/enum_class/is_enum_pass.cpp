fd292518e9065db2bdfb477bcf7d7d914f661be1
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-21 09:29:14 +0000

    boost::is_enum test showing the bug
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/enums@73978 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/enums/test/enum_class/is_enum_pass.cpp
+++ libs/enums/test/enum_class/is_enum_pass.cpp
@@ -12,12 +12,22 @@
 
 #include "./EnumClass.hpp"
 #include <boost/static_assert.hpp>
-#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/is_enum.hpp>
+
+//enum E {E_a,E_b,E_c};
+//BOOST_STATIC_ASSERT((boost::is_enum<E>::value));
+//enum class EC {a,b,c};
+//BOOST_STATIC_ASSERT((boost::is_enum<EC>::value));
+
+//#include <type_traits>
+//BOOST_STATIC_ASSERT((std::is_enum<E>::value));
+//BOOST_STATIC_ASSERT((std::is_enum<EC>::value));
 
 void pass() {
   using namespace boost::enums;
 
   { // has the same size as the underlying type
+    //BOOST_STATIC_ASSERT((std::is_enum<EnumClass>::value));
     BOOST_STATIC_ASSERT((boost::enums::is_enum<EnumClass>::value));
     BOOST_STATIC_ASSERT((!boost::enums::is_enum<int>::value));
   }
